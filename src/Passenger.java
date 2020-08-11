2cdab5f0753641468afea574e5fdf3880259a40f
Hououin47 <44270164+Hououin47@users.noreply.github.com>
2020-08-11 23:21:56 +0200

    Update Passenger.java
    comments

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,17 +1,21 @@
 public class Passenger extends Person {
 
     String email;
-
+    
+    // constructor
     Passenger (String email, String name, String surname,String phone_number,
                int cash) {
+        //call super (person) constructor
         super(name,surname,phone_number, cash);
         this.email = email;
     }
 
+    //setters
     public void setEmail(String email){
         this.email  = email;
     }
-
+    
+    //getters
     public String getEmail() {
         return this.email;
     }
